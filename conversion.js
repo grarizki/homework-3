88d77e470f314a3d0bd23ca6f4b633fc3b6f1461
Raka Grarizki <28901232+grarizki.noreply.github.com>
2021-08-21 23:42:53 +0700

    add conversion.js

--- conversion.js
+++ conversion.js
@@ -1,3 +1,18 @@
+// Setup Conversion
+const celciusInput = document.getElementById("celcius-in");
+const fahrenheitInput = document.getElementById("fahrenheit-in");
+const kelvinInput = document.getElementById("kelvin-in");
+const celciusOutput = document.getElementById("celcius-out");
+const fahrenheitOutput = document.getElementById("fahrenheit-out");
+const kelvinOutput = document.getElementById("kelvin-out");
+
+console.log(celciusInput)
+console.log(fahrenheitInput)
+console.log(kelvinInput)
+console.log(celciusOutput)
+console.log(fahrenheitOutput)
+console.log(kelvinOutput)
+
 // Converting Celcius to Celcius
 
 // Converting Celcius to Fahrenheit
