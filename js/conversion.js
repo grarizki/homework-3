421b0e0705c5b2fe9dd2b8a2199ba4ed6a7587cb
Raka Grarizki <28901232+grarizki.noreply.github.com>
2021-08-21 20:01:56 +0700

    add prep for conversion.js

---
+++ js/conversion.js
@@ -0,0 +1,22 @@
+// Converting Celcius to Celcius
+
+// Converting Celcius to Fahrenheit
+
+// Converting Celcius to Kelvin
+
+
+
+
+// Converting Fahrenheit to Celcius
+
+// Converting Fahrenheit to Fahrenheit
+
+// Converting Fahrenheit to Kelvin
+
+
+
+// Converting Kelvin to Celcius
+
+// Converting Kelvin to Fahrenheit
+
+// Converting Kelvin to Kelvin
