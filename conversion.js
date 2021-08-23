e55035094bfec347cf0e0b7cd3b1c68d0cbe20e5
Raka Grarizki <28901232+grarizki.noreply.github.com>
2021-08-23 18:05:13 +0700

    fix error conversion

--- conversion.js
+++ conversion.js
@@ -1,51 +1,54 @@
 // Setup Conversion
-class Conversion { constructor() {
-    constructor()
-    fromCelcius(to, temp-input);{
+class Conversion { 
+    constructor(){
+        console.log("test");
+    }
+    fromCelcius(to, temp_input){
         // Converting Celcius to Celcius
         if (to == "Cel") {
-            return temp-input;
+            return temp_input;
         } 
         // Converting Celcius to Fahrenheit
         if (to == "Fah") {
-            return ( 9 / 5 ) * (temp-input + 32);
+            return ( 9 / 5 ) * (temp_input + 32);
         }
         // Converting Celcius to Reamur
         if (to == "Rea") {
-            return ( 4 / 5 ) * (temp-input +32);
+            return ( 4 / 5 ) * (temp_input +32);
         }
     }
     
-    fromFahrenheit(to, temp-input);{
+    fromFahrenheit(to, temp_input){
         // Converting Fahrenheit to Fahrenheit
         if (to == "Fah") {
-            return temp-input;
+            return temp_input;
         }
         // Converting Fahrenheit to Celcius
         if (to == "Cel") {
-            return ( 5 / 9 ) * (temp-input + 32);
+            return ( 5 / 9 ) * (temp_input + 32);
         }
         // Converting Fahrenheit to Reamur
         if (to == "Rea") {
-            return ( 4 / 9 ) * (temp-input - 32);
+            return ( 4 / 9 ) * (temp_input - 32);
+        }
     }
-    }
-    fromReamur(to, temp-input);{
+    fromReamur(to, temp_input){
     
         if (to == "Rea") {
             // Converting Reamur to Reamur
-            return temp-input;
+            return temp_input;
         }
         // Converting Reamur to Celcius
         if (to == "Cel") {
-            return ( 5 / 4 ) * (temp-input);
+            return ( 5 / 4 ) * (temp_input);
         }
         // Converting Reamur to Fahrenheit
         if (to == "Fah") {
-            return ( 9 / 4 ) * (temp-input);
+            return ( 9 / 4 ) * (temp_input);
         }
     }
 }
-
-
 module.exports = Conversion;
+
+
+modules.exports = Conversion;
