21c08c91754e1601d18958703fad5858ca60bec6
Raka Grarizki <28901232+grarizki.noreply.github.com>
2021-08-23 19:28:31 +0700

    moved to js folder

---
+++ js/conversion.js
@@ -0,0 +1,53 @@
+// Setup Conversion
+class Conversion { 
+    constructor(){
+        console.log("test");
+    }
+    fromCelcius(to, temp_input){
+        // Converting Celcius to Celcius
+        if (to == "Cel") {
+            return temp_input;
+        } 
+        // Converting Celcius to Fahrenheit
+        if (to == "Fah") {
+            return  (9 / 5) * temp_input + 32;
+        }
+        // Converting Celcius to Reamur
+        if (to == "Rea") {
+            return  (4 / 5) * temp_input;
+        }
+    }
+    
+    fromFahrenheit(to, temp_input){
+        // Converting Fahrenheit to Fahrenheit
+        if (to == "Fah") {
+            return temp_input;
+        }
+        // Converting Fahrenheit to Celcius
+        if (to == "Cel") {
+            return  (5 / 9) * (temp_input - 32);
+        }
+        // Converting Fahrenheit to Reamur
+        if (to == "Rea") {
+            return  (4 / 9) * (temp_input - 32);
+        }
+    }
+    fromReamur(to, temp_input){
+    
+        if (to == "Rea") {
+            // Converting Reamur to Reamur
+            return temp_input;
+        }
+        // Converting Reamur to Celcius
+        if (to == "Cel") {
+            return  (5 / 4) * temp_input;
+        }
+        // Converting Reamur to Fahrenheit
+        if (to == "Fah") {
+            return  (9 / 4) * temp_input + 32;
+        }
+    }
+}
+
+
+modules.exports = Conversion;
